7dcf83a86fa3fcaa223431f8dfcf491743b63f34
Lena Hou <lenahou2013@gmail.com>
2021-08-05 14:48:18 -0400

    commit tree 2df87f674e3d7c19b94e750fab87a93746d493fc

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,16 @@ Rails.application.routes.draw do
 
   resources :posts, only: %i[index show new create edit update]
 
-  get '/admin/stats', to: 'stats#index'
+  # Scoping allows us to prefix a block of routes under one grouping
+  # routes should be DRY too!!!
+  # the new /admin/stats helpers are stats_path and stats_url
+  # We are telling Rails that we want to use /admin as a URL prefix
+  # We also let Rails know that all of the included routes will be handled by controllers in the admin module 
+  # If you switch from scope to namespace, take care to update any URL helpers you have in use
+
+  namespace :admin do
+    resources :stats, only: [:index]
+  end
 
   root 'posts#index'
 end
